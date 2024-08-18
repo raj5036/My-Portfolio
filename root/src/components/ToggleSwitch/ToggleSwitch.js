6bad4582c1ea8caf0367670e588d2bd04390e69f
Raj Karmakar <97rajmath@gmail.com>
2024-08-18 16:29:40 +0530

    Change theme toggle button

--- root/src/components/ToggleSwitch/ToggleSwitch.js
+++ root/src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,23 +1,32 @@
 import React, {useState, useContext} from "react";
 import StyleContext from "../../contexts/StyleContext";
 import "./ToggleSwitch.scss";
+import SunIcon from "../../assets/svgs/SunIcon.svg"
+import MoonIcon from "../../assets/svgs/MoonIcon.svg"
 
 const ToggleSwitch = () => {
   const {isDark} = useContext(StyleContext);
   const [isChecked, setChecked] = useState(isDark);
   const styleContext = useContext(StyleContext);
 
+  const onClick = () => {
+    styleContext.changeTheme();
+    setChecked(!isChecked);
+  };
+
   return (
     <label className="switch">
-      <input
-        type="checkbox"
-        checked={isDark}
-        onChange={() => {
-          styleContext.changeTheme();
-          setChecked(!isChecked);
-        }}
-      />
-      <span className="slider round"></span>
+      {isDark ? (<img
+        src={SunIcon}
+        alt="Sun Icon"
+        className="icon sun"
+        onClick={onClick}
+      />) : (<img
+        src={MoonIcon}
+        alt="Moon Icon"
+        className="icon moon"
+        onClick={onClick}
+      />)}
     </label>
   );
 };
