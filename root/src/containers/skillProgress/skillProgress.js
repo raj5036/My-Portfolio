1b97e986d8f3cb6d66a44f32797a395d062331b4
Raj Karmakar <97rajmath@gmail.com>
2025-07-11 12:58:18 +0530

    [WIP] update work proficiency section

--- root/src/containers/skillProgress/skillProgress.js
+++ root/src/containers/skillProgress/skillProgress.js
@@ -4,38 +4,35 @@ import {illustration, techStack} from "../../portfolio";
 import {Fade} from "react-reveal";
 import Build from "../../assets/lottie/build";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import ReactIcon from "../../assets/svgs/ReactIcon.svg"
+import GitIcon from "../../assets/svgs/GitIcon.svg"
+import ServerIcon from "../../assets/svgs/ServerIcon.svg"
 
 export default function StackProgress() {
   if (techStack.viewSkillBars) {
     return (
       <Fade bottom duration={1000} distance="20px">
         <div className="skills-container">
-          <div className="skills-bar">
-            <h1 className="skills-heading">Proficiency</h1>
-            {techStack.experience.map((exp, i) => {
-              const progressStyle = {
-                width: exp.progressPercentage
-              };
-              return (
-                <div key={i} className="skill">
-                  <p>{exp.Stack}</p>
-                  <div className="meter">
-                    <span style={progressStyle}></span>
-                  </div>
-                </div>
-              );
-            })}
+          <div className="grid-item frontend">
+            <img src={ReactIcon} alt="React Icon"/>
+            <div>
+              <p className="skill-type">Frontend</p>
+              <p className="skill-list">React, Redux, Zustand, React-Query, Tailwind, Angular</p>
+            </div>
           </div>
-
-          <div className="skills-image">
-            {illustration.animated ? (
-              <DisplayLottie animationData={Build} />
-            ) : (
-              <img
-                alt="Skills"
-                src={require("../../assets/images/skill.svg")}
-              />
-            )}
+          <div className="grid-item version-control">
+            <img src={GitIcon} alt="Git Icon"/>
+            <div>
+              <p className="skill-type">Version Control</p>
+              <p className="skill-list">Git, GitHub</p>
+            </div>
+          </div>
+          <div className="grid-item backend">
+            <img src={ServerIcon} alt="Server Icon"/>
+            <div>
+              <p className="skill-type">Backend</p>
+              <p className="skill-list">NodeJs, ExpressJs, Golang, PHP, Laravel</p>
+            </div>
           </div>
         </div>
       </Fade>
