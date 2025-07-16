71182aa4a916efb5be57196be0ff4a9e5cc511ce
Raj Karmakar <97rajmath@gmail.com>
2025-07-16 12:57:36 +0530

    chore: Minor changes

--- root/src/containers/skillProgress/skillProgress.js
+++ root/src/containers/skillProgress/skillProgress.js
@@ -14,41 +14,43 @@ export default function StackProgress() {
   if (techStack.viewSkillBars) {
     return (
       <Fade bottom duration={1000} distance="20px">
-        <h1 className="skills-heading">Proficiency</h1>
-        <div className="skills-container">
-          <div className="grid-item frontend">
-            <img src={ReactIcon} alt="React Icon"/>
-            <div>
-              <p className="skill-type">Frontend</p>
-              <p className="skill-list">React, Redux, Zustand, React-Query, Tailwind, Angular</p>
+        <div className="main">
+          <h1 className="skills-heading">Proficiency</h1>
+          <div className="skills-container">
+            <div className="grid-item frontend">
+              <img src={ReactIcon} alt="React Icon"/>
+              <div className="skill">
+                <p className="skill-type">Frontend</p>
+                <p className="skill-list">React, Redux, Zustand, React-Query, Tailwind, Angular</p>
+              </div>
             </div>
-          </div>
-          <div className="grid-item version-control">
-            <img src={GitIcon} alt="Git Icon"/>
-            <div>
-              <p className="skill-type">Version Control</p>
-              <p className="skill-list">Git, GitHub</p>
+            <div className="grid-item version-control">
+              <img src={GitIcon} alt="Git Icon"/>
+              <div className="skill">
+                <p className="skill-type">Version Control</p>
+                <p className="skill-list">Git, GitHub</p>
+              </div>
             </div>
-          </div>
-          <div className="grid-item backend">
-            <img src={ServerIcon} alt="Server Icon" style={{ height: "5rem" }}/>
-            <div>
-              <p className="skill-type">Backend</p>
-              <p className="skill-list">NodeJs, ExpressJs, Golang, PHP, Laravel, GraphQL, NestJs</p>
+            <div className="grid-item backend">
+              <img src={ServerIcon} alt="Server Icon"/>
+              <div className="skill">
+                <p className="skill-type">Backend</p>
+                <p className="skill-list">NodeJs, ExpressJs, Golang, PHP, Laravel, GraphQL, NestJs</p>
+              </div>
             </div>
-          </div>
-          <div className="grid-item database">
-            <img src={DatabaseIcon} alt="Database Icon"/>
-            <div>
-              <p className="skill-type">Databases</p>
-              <p className="skill-list">MySQL, MongoDB, Postgres, SQL server</p>
+            <div className="grid-item database">
+              <img src={DatabaseIcon} alt="Database Icon"/>
+              <div className="skill">
+                <p className="skill-type">Databases</p>
+                <p className="skill-list">MySQL, MongoDB, Postgres, SQL server</p>
+              </div>
             </div>
-          </div>
-          <div className="grid-item cloud">
-            <img src={CloudIcon} alt="Cloud Icon"/>
-            <div>
-              <p className="skill-type">Cloud</p>
-              <p className="skill-list">AWS (EC2, S3, RDS), Azure (Virtual Machine, Azure Data Studio)</p>
+            <div className="grid-item cloud">
+              <img src={CloudIcon} alt="Cloud Icon"/>
+              <div className="skill">
+                <p className="skill-type">Cloud</p>
+                <p className="skill-list">AWS (EC2, S3, RDS), Azure (Virtual Machine, Azure Data Studio)</p>
+              </div>
             </div>
           </div>
         </div>
